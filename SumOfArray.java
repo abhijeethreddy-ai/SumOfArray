9d593bf20f1acaa4c59ea79f1d7cb9160ef08c54
Abhijeeth Reddy <reddy.abhijeeth@gmail.com>
2025-04-23 10:54:42 -0400

    Add files via upload

---
+++ SumOfArray.java
@@ -0,0 +1,25 @@
+package day7;
+
+public class SumOfArray {
+
+	public static void main(String[] args) {
+		
+		        // Example array
+		        int[] numbers = {50, 60, 70, 80, 90};
+
+		        // Variable to store the sum
+		        int sum = 0;
+
+		        // for loop to the array to calculate sum
+		        for (int num : numbers) {
+		            sum += num;
+		        }
+
+		        // Print output
+		        System.out.println("The sum of the array elements is: " + sum);
+		    }
+		}
+
+	
+
+
